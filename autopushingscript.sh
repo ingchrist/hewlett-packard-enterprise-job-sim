fdbc61580dbeebc56b524bd9d8107ce4e17acf3c
ingchrist <127639176+ingchrist@users.noreply.github.com>
2025-05-26 23:14:33 +0000

    in the mist of doing hard things by ingchrist: file/folder change detected

--- autopushingscript.sh
+++ autopushingscript.sh
@@ -12,5 +12,7 @@ while inotifywait -r -e modify,create,delete,move .; do
     git commit -m "in the mist of doing hard things by ingchrist: file/folder change detected"
     git push
     echo "Changes pushed to GitHub."
+    echo "Sleeping for one minute..."
     sleep 60
+    echo "Watching for changes again..."
 done

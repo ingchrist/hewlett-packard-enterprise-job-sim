60e1a63928537924746801e8faf588a2266b0b68
ingchrist <127639176+ingchrist@users.noreply.github.com>
2025-05-27 00:33:37 +0000

    in the mist of doing hard things by ingchrist

--- src/main/java/com/hpe/employees/EmployeeServiceApplication.java
+++ src/main/java/com/hpe/employees/EmployeeServiceApplication.java
@@ -3,9 +3,15 @@ package com.hpe.employees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class EmployeeServiceApplication {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+        logger.info("Employee Service Application started successfully.");
     }
 }
